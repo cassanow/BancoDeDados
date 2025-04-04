207163acfb0490f433bbec4282491f94cbce2f5d
Arthur <nashcassano8@gmail.com>
2025-04-04 16:55:50 -0300

    Adicionando a minha PessoaController

---
+++ Controllers/PessoaController.cs
@@ -0,0 +1,19 @@
+﻿using CadastroUsuarios.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CadastroUsuarios.Controllers
+{
+    public class PessoaController : Controller
+    {
+        private readonly Conexao _context;
+        public PessoaController(Conexao context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var pessoas = _context.Pessoa.ToList();
+            return View();
+        }
+    }
+}
