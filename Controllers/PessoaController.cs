f7d360327e6b751c008816a35f5e298f0a60d21e
Arthur <nashcassano8@gmail.com>
2025-04-16 16:29:19 -0300

    Finalizando meu CRUD

--- Controllers/PessoaController.cs
+++ Controllers/PessoaController.cs
@@ -1,5 +1,8 @@
-﻿using CadastroUsuarios.Models;
+﻿using System.Text.Json;
+using CadastroUsuarios.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
 
 namespace CadastroUsuarios.Controllers
 {
@@ -10,30 +13,73 @@ namespace CadastroUsuarios.Controllers
         {
             _context = context;
         }
+        
+        [HttpGet]
         public IActionResult Criar()
         {
-            return View();
+            
+            return View((new Pessoa()));
         }
 
         [HttpPost]
         public async Task<IActionResult> Criar(Pessoa pessoa)
         {
+            
             if (ModelState.IsValid)
             {
+                if (pessoa.ID > 0)
+                {
+                    _context.Entry(pessoa).State = EntityState.Modified;
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction("PessoasCadastradas");
+                }
                 await _context.Pessoa.AddAsync(pessoa);
                 await _context.SaveChangesAsync();
                 return RedirectToAction("PessoasCadastradas");
             }
-            return View(pessoa);
+            return View("Criar", pessoa);
         }
 
         public IActionResult Editar()
         {
             return View();
         }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Editar(int id)
+        {
+           
+            var pessoa = await _context.Pessoa.FindAsync(id);
+            if (pessoa == null)
+            {
+                ModelState.AddModelError("", "Pessoa não encontrada!");
+                return View();  
+            }
+            return View("Criar", pessoa);    
+        }
+
         public IActionResult Remover()
         {
             return View();
+        }
+        
+        [HttpPost]  
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Remover(int id)
+        {
+              var pessoa = await _context.Pessoa.FirstOrDefaultAsync(p => p.ID == id);
+              if (pessoa != null)
+              {
+                  _context.Pessoa.Remove(pessoa);   
+              }
+              else
+              {
+                  return View("Remover");
+                  
+              }
+              await _context.SaveChangesAsync();
+              return RedirectToAction("PessoasCadastradas");
         }
         public IActionResult PessoasCadastradas()
         {
