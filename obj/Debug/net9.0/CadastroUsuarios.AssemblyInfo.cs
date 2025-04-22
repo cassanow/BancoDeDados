510a2c453ed22b15d8dae43ba87b857cf3275104
Arthur <nashcassano8@gmail.com>
2025-04-22 13:50:04 -0300

    Finalizando o meu filtro d busca

--- obj/Debug/net9.0/CadastroUsuarios.AssemblyInfo.cs
+++ obj/Debug/net9.0/CadastroUsuarios.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CadastroUsuarios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7d360327e6b751c008816a35f5e298f0a60d21e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfa37d25ee9dbbeb0578918c8fab077ddcc423ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("CadastroUsuarios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CadastroUsuarios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,9 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+﻿let table = new DataTable('#table-contacts', {
+    language: {
+        url: '//cdn.datatables.net/plug-ins/1.13.4/i18n/pt-BR.json'
+    }
+});
+// Please see documentation at
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
